004da817a3687ba76f95c59453e183bdef369420
Sepideh Miralaei <sepidehmiralaei@Sepidehs-MacBook-Pro.local>
2020-09-28 10:30:43 -0400

    add bootstrap navbar and edit layouts

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'pages/about'
   get 'pages/contact'
   resources :clock_events
+  root 'clock_events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
