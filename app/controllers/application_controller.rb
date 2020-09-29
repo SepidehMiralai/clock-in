83fc873b80f2b6f77773bbd32862e8628da1e966
Sepideh Miralaei <sepidehmiralaei@Sepidehs-MacBook-Pro.local>
2020-09-29 17:45:42 -0400

    add buttons for Clock In/Out and delete footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_current_user
+  
 
   private
 
   def set_current_user
     Current.user = current_user
   end
+
 
   protected
 
